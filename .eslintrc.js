d0387376e39a897e6bec1e82320035d6a16544e9
Tiago Boeing <contato@tiagoboeing.com>
2023-06-29 15:32:30 -0300

    ESLint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,29 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: ['standard'],
+  extends: 'standard-with-typescript',
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 12,
+    project: ['./tsconfig.json'],
+    ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: ['@typescript-eslint'],
-  rules: {}
+  rules: {
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'warn',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off'
+  }
 }
