dbb156d4788e159c84b249866580b6c55a5f6bc0
Tiago Boeing <contato@tiagoboeing.com>
2023-06-29 15:29:49 -0300

    feat: :stethoscope: Simple `/healthcheck` endpoint to be used on Kubernetes probes, like `liveness/readiness`

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,18 +9,23 @@ module.exports = {
       env: {
         node: true
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
         sourceType: 'script'
       }
     }
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
+    project: ['./tsconfig.json'],
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
   rules: {
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'warn',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off'
   }
 }
